f113c22a593db259b54093e49a0f03e63b711caf
homemix <kenmutati@gmail.com>
2022-06-15 10:24:34 +0300

    Fix error:3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,14 +3,12 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all(
     [
-      uploadPhoto().then(result => {
-        console.log(`${result.body} `);
-      }),
-      createUser().then(
-        (user) => {
-          console.log(`${user.firstName} ${user.lastName}`);
-        },
-      ),
+      uploadPhoto(),
+      createUser(),
     ],
+  ).then(
+    ([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    },
   ).catch(() => new Error('Signup system offline'));
 }
