6ee91ecdee126ed41d8b86d1a18e3c41faf666cb
homemix <kenmutati@gmail.com>
2022-06-16 14:16:33 +0300

    3. Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw TypeError('name must be a string');
+    if (typeof code !== 'string') throw TypeError('code must be a string');
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._code} (${this._name})`;
+  }
+}
