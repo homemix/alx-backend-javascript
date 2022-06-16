c44058c70a4102482a9a27ecc9082237ea261800
homemix <kenmutati@gmail.com>
2022-06-16 15:36:13 +0300

    10. Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+export default class Car{
+    constructor(brand,motor,color) {
+        if (typeof brand !== 'string') throw new Error('Brand must be a string');
+        if (typeof motor !== 'string') throw new Error('Motor must be a string');
+        if (typeof color !== 'string') throw new Error('Color must be a string');
+
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+    cloneCar(){
+        //  return a new object of the class
+        return new Car(this._brand,this._motor,this._color);
+    }
+}
