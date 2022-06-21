333f7b4190bf524e0dcd4ccaf3adb052806f7c10
homemix <kenmutati@gmail.com>
2022-06-21 17:32:38 +0300

    Fix : 8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,11 @@
 export default function cleanSet(set, startString) {
+  const strings = [];
   if (startString && typeof startString === 'string') {
-    const strings = [];
     for (const item of set) {
       if (item.startsWith(startString)) {
         strings.push(item.slice(startString.length));
       }
     }
-    return strings.join('-');
   }
-  return '';
+  return strings.join('-');
 }
