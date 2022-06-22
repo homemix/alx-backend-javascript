54ec318d43ec2f02e5ec4bb8198d19d3f2a814fd
homemix <kenmutati@gmail.com>
2022-06-22 15:37:04 +0300

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,16 +1,39 @@
-const director1 = {
+function printTeacher(firstName, lastName) {
+    // a function to print teacher
+    // with arguments firstName and lastName
+    return "".concat(firstName.slice(0, 1), ". ").concat(lastName);
+}
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    return StudentClass;
+}());
+// testing and examples
+// test director interface
+var director1 = {
     firstName: 'John',
     lastName: 'Doe',
     location: 'London',
     fullTimeEmployee: true,
-    numberOfReports: 17,
+    numberOfReports: 17
 };
 console.log(director1);
-const teacher3 = {
+// test teacher interface
+var teacher3 = {
     firstName: 'John',
     fullTimeEmployee: false,
     lastName: 'Doe',
     location: 'London',
-    contract: false,
+    contract: false
 };
 console.log(teacher3);
+// test print teacher function
+console.log(printTeacher('John', 'Doe'));
+// test class student
+var student1 = new StudentClass('John', 'Doe');
+console.log(student1.workOnHomework());
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,6 @@
+
 interface Teacher{
+    // an interface for teacher
     firstName:string
     fullTimeEmployee:boolean
     lastName:string
@@ -6,10 +8,44 @@ interface Teacher{
     contract?:boolean
 }
 
+
 interface Directors extends Teacher{
+    // an interface for director extending teacher
     numberOfReports:number;
 }
 
+function printTeacher(firstName:string, lastName:string){
+    // a function to print teacher
+// with arguments firstName and lastName
+    return`${firstName.slice(0,1)}. ${lastName}`
+}
+
+interface printTeacherFunction{
+    // an interface for the function print teacher
+    (firstName:string, lastName:string):string
+}
+
+interface Student{
+    // an interface for student class
+    firstName:string
+    lastName:string
+}
+class StudentClass implements Student{
+// a class for student class
+    firstName: string;
+    lastName: string;
+    constructor(firstName:string, lastName:string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework():string{
+        return `Currently working`
+    }
+}
+
+
+// testing and examples
+// test director interface
 const director1:Directors = {
     firstName: 'John',
     lastName: 'Doe',
@@ -19,6 +55,7 @@ const director1:Directors = {
 }
 console.log(director1);
 
+// test teacher interface
 const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
@@ -27,3 +64,10 @@ const teacher3: Teacher = {
     contract: false,
 };
 console.log(teacher3);
+
+// test print teacher function
+console.log(printTeacher('John','Doe'))
+
+// test class student
+const student1: StudentClass = new StudentClass('John','Doe');
+console.log(student1.workOnHomework());
